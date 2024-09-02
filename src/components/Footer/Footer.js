47f4863bc34fe6a7ac4accbf79d4683b0cd34759
Renan <renanvgmartins@hotmail.com>
2024-09-02 19:06:10 -0300

    Alterações no Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,14 +5,16 @@ import { Link } from 'react-router-dom'
 export default function Footer(){
     return(
         <div className="footer">
-            <p>Powered by Renan <FiHeart size={20} /></p>
-            <div className='footer-links'>
-                <Link className='footer-link' to={'https://github.com/Valverd'} target='_blank'>
-                    <FiGithub size={20}/>
-                </Link>
-                <Link className='footer-link' to={'https://www.linkedin.com/in/renan-valverde/'} target='_blank'>
-                    <FiLinkedin size={20}/>
-                </Link>
+            <div className="footer-content">
+                <p>Powered by Renan <FiHeart size={20} /></p>
+                <div className='footer-links'>
+                    <Link className='footer-link' to={'https://github.com/Valverd'} target='_blank'>
+                        <FiGithub size={20}/>
+                    </Link>
+                    <Link className='footer-link' to={'https://www.linkedin.com/in/renan-valverde/'} target='_blank'>
+                        <FiLinkedin size={20}/>
+                    </Link>
+                </div>
             </div>
         </div>
     )
